d56d3f78708923cd6e0ec29e9faa408e8e83619f
Daniël de Kok <me@danieldk.eu>
2022-06-16 13:37:08 +0200

    AppleOps.gemm: write in-place when `output` is given (#19)
    * AppleOps.gemm: write in-place when `output` is given
    
    NumpyOps.gemm (with BLIS) writes the result of matrix multiplication
    in-place when the `output` argument is given. This changes AppleOps.gemm
    to do the same, avoiding allocation of a temporary.
    
    * blas.gemm: move `out` kwarg to the end
    
    To retain API compatibility.

--- thinc_apple_ops/blas.pyx
+++ thinc_apple_ops/blas.pyx
@@ -3,18 +3,29 @@ cimport numpy as np
 import numpy
 
 
-cpdef np.ndarray gemm(float[:, ::1] A, float[:, ::1] B, bint trans1=False, bint trans2=False): 
+cpdef np.ndarray gemm(float[:, ::1] A, float[:, ::1] B,
+                      bint trans1=False, bint trans2=False,
+                      np.ndarray out=None):
     cdef int nM = A.shape[0] if not trans1 else A.shape[1]
     cdef int nK = A.shape[1] if not trans1 else A.shape[0]
     cdef int nK_b = B.shape[0] if not trans2 else B.shape[1]
     cdef int nN = B.shape[1] if not trans2 else B.shape[0]
-    cdef np.ndarray out = numpy.empty((nM, nN), dtype="f")
+
+    cdef float[:, ::1] C = out
+
+    if out is None:
+        out = numpy.empty((nM, nN), dtype="f")
+        C = out
+    else:
+        if C.shape[0] != nM or C.shape[1] != nN:
+            msg = "Shape mismatch for output matrix, was: (%d, %d), expected (%d, %d)"
+            raise ValueError(msg % (C.shape[0], C.shape[1], nM, nN))
+
 
     if nK != nK_b:
         msg = "Shape mismatch for gemm: (%d, %d), (%d, %d)"
         raise ValueError(msg % (nM, nK, nK_b, nN))
 
-    cdef float[:, ::1] C = out
     if nM == 0 or nK == 0 or nN == 0:
         return out
 
